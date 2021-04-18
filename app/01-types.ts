4c6f70004e3e796effe5dcdd559ca320ef1706d7
Patrick Cook <ptcook74@gmail.com>
2021-04-18 14:50:03 -0400

    first push

---
+++ app/01-types.ts
@@ -0,0 +1,34 @@
+//1   //2  //3
+let username: string = "instructor";
+// let password: string = "Letmein1234!"
+let instructorId: number = 5;
+
+let password: string;
+let phoneNumber: number;
+let email: string;
+let socialSecurity: number;
+let isMarried: boolean;
+
+let greeting = "Hello";
+//Number
+let age: number = 40;
+let currentYear: number = 2018
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+//String
+let companyName: string;
+companyName = "Eleven Fifty"
+
+let appName: string = 'Twitter';
+appName = "Instagram";
+//Boolean
+let isLoggedIn: boolean = true;
+let isGoldLevelUser: boolean = true;
+//Array
+let studentList:  string[] = ['Tom', 'Tracy', 'Nikaya']
+let allScores: Array<number> = [100, 92, 95, 96];
+
+console.log("Student List:", studentList[0]);
+
---
+++ js/01-types.js
@@ -0,0 +1,29 @@
+//1   //2  //3
+var username = "instructor";
+// let password: string = "Letmein1234!"
+var instructorId = 5;
+var password;
+var phoneNumber;
+var email;
+var socialSecurity;
+var isMarried;
+var greeting = "Hello";
+//Number
+var age = 40;
+var currentYear = 2018;
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+//String
+var companyName;
+companyName = "Eleven Fifty";
+var appName = 'Twitter';
+appName = "Instagram";
+//Boolean
+var isLoggedIn = true;
+var isGoldLevelUser = true;
+//Array
+var studentList = ['Tom', 'Tracy', 'Nikaya'];
+var allScores = [100, 92, 95, 96];
+console.log("Student List:", studentList[0]);
+//# sourceMappingURL=01-types.js.map
